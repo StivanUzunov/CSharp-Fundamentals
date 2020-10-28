27039466782a5f467baa81886f69993eefd981cb
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-10-28 14:09:58 +0200

    submit

---
+++ Arrays/MagicSum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[i] + array[j] == n)
+                {
+                    Console.WriteLine($"{array[i]} {array[j]}");
+                }
+            }
+        }
+    }
+
+}
---
+++ Arrays/MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int maxLength = 0;
+        int length = 1;
+
+        int start = 0;
+        int bestStart = 0;
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            if (array[i] == array[i + 1])
+            {
+                length++;
+            }
+            else
+            {
+                length = 1;
+                start = i + 1;
+            }
+
+            if (length > maxLength)
+            {
+                maxLength = length;
+                bestStart = start;
+            }
+        }
+
+        for (int i = bestStart; i < bestStart + maxLength; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+
+}
