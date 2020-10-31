a0921796428b3c90c014c3210e64b3598eef7b8a
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-10-31 17:02:57 +0200

    submit

---
+++ ObjectAndClasses/AdvertisementMessage/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                AdvertisementMessage ad = new AdvertisementMessage();
+                Console.WriteLine(ad.GenerateMessage());
+            }
+        }
+    }
+    public class AdvertisementMessage
+    {
+        public string[] Phrases = new string[] {"Excellent product.", "Such a great product.",
+            "I always use that product.", "Best product of its category.",
+            "Exceptional product.", "I can’t live without this product."};
+        public string[] Events = new string[] {"Now I feel good.", "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.", "I feel great!"};
+        public string[] Authors = new string[] { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+        public string[] Cities = new string[] { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+        public string GenerateMessage()
+        {
+            Random rand = new Random();
+            string currentphrase = Phrases[rand.Next(0, Phrases.Length)];
+            string currentEvents = Events[rand.Next(0, Events.Length)];
+            string currentAuthor = Authors[rand.Next(0, Authors.Length)];
+            string currentCities = Cities[rand.Next(0, Cities.Length)];
+            return $"{currentphrase} {currentEvents} {currentAuthor} – {currentCities}";
+        }
+    }
+}
---
+++ ObjectAndClasses/OrderByAge/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Person> people = new List<Person>();
+            string[] command = new string[2];
+            while (command[0] != "End")
+            {
+                command = Console.ReadLine().Split().ToArray();
+                if (command[0] == "End")
+                {
+                    break;
+                }
+
+                Person student = new Person(command[0], command[1], int.Parse(command[2]));
+                people.Add(student);
+            }
+            Console.WriteLine(String.Join(Environment.NewLine,
+                people.OrderBy(x => x.YearsOld)));
+
+        }
+    }
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string ID { get; set; }
+        public int YearsOld { get; set; }
+        public Person(string firstname, string id, int yearsold)
+        {
+            this.FirstName = firstname;
+            this.ID = id;
+            this.YearsOld = yearsold;
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} with ID: {ID} is {YearsOld} years old.";
+        }
+    }
+
+}
