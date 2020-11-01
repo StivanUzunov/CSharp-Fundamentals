b4d78011c7741ded3cf33a404304ee0dd6201e4f
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-11-01 20:44:01 +0200

    submit

---
+++ AssociativeArrays/MinerTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Xml.Serialization;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resourceQuantity = new Dictionary<string, int>();
+            while (true)
+            {
+                string resource = Console.ReadLine();
+                if (resource == "stop")
+                {
+                    break;
+                }
+                int quantity = int.Parse(Console.ReadLine());
+                if (resourceQuantity.ContainsKey(resource))
+                {
+                    resourceQuantity[resource] += quantity;
+                }
+                else
+                {
+                    resourceQuantity.Add(resource, quantity);
+                }
+            }
+            foreach (var pair in resourceQuantity)
+            {
+                Console.WriteLine(pair.Key + " -> " + pair.Value);
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
