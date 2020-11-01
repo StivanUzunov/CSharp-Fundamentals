8de6f21ab6cc6ef415112c2c1dc942b748661782
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-11-01 20:51:11 +0200

    submit

---
+++ AssociativeArrays/Courses/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Xml.Serialization;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var courses = new Dictionary<string, List<string>>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+                string courseName = input.Split(" : ")[0];
+                string studentName = input.Split(" : ")[1];
+
+                if (!courses.ContainsKey(courseName))
+                {
+                    courses.Add(courseName, new List<string>());
+                    courses[courseName].Add(studentName);
+
+                }
+                else
+                {
+                    courses[courseName].Add(studentName);
+                }
+            }
+            foreach (var pair in courses.OrderByDescending(course => course.Value.Count))
+            {
+                Console.WriteLine(pair.Key + ": " + pair.Value.Count);
+                List<string> students = pair.Value;
+                students.Sort();
+                foreach (var student in students)
+                {
+                    Console.WriteLine("-- " + student);
+                }
+            }
+
+
+        }
+    }
+}
