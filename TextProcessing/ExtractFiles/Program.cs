e49079e247bc6244a4910c0e8bccc0d1eb1619a3
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-11-02 17:50:56 +0200

    submit

---
+++ TextProcessing/ExtractFiles/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split('\\', StringSplitOptions.RemoveEmptyEntries);
+            var file = input[input.Length - 1].Split('.');
+            Console.WriteLine($"File name: {file[0]}");
+            Console.WriteLine($"File extension: {file[1]}");
+
+
+
+
+
+        }
+
+
+    }
+}
+
