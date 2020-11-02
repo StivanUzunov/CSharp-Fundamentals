bdb43365a5cd5a2a8696d59fa87a0a39fa054f6e
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-11-02 17:59:07 +0200

    submit

---
+++ TextProcessing/ReplaceRepeatingCharacter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.XPath;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string result = String.Empty;
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (!(input[i] == input[i + 1]))
+                {
+                    result += input[i];
+                }
+
+            }
+            result += input[input.Length - 1];
+            Console.WriteLine(result);
+        }
+    }
+}
