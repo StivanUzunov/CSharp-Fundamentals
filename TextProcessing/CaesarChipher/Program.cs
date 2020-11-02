f4afc8634d92b7bfd83a983bbb27862fd969b6c0
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-11-02 17:53:04 +0200

    submit

---
+++ TextProcessing/CaesarChipher/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string encrypted = String.Empty;
+            string text = Console.ReadLine();
+            for (int i = 0; i < text.Length; i++)
+            {
+                encrypted += (char)(text[i] + 3);
+            }
+            Console.WriteLine(encrypted);
+
+
+
+
+        }
+
+
+    }
+}
